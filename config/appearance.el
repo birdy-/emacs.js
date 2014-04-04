86583a7e48783a5c5b38a69fe6f34456541632c9
Clément 'birdy' Danjou <clement.danjou@honeyvinegar.fr>
2014-04-04 11:57:32 +0200

    Adding save of frame buffer

--- config/appearance.el
+++ config/appearance.el
@@ -24,3 +24,44 @@
                                (highlight-lines-matching-regexp ".\\{81\\}" "hi-blue-b")
                            )
           )
+
+(defmacro defconstant (symbol initvalue &optional docstring)
+  `(defconst ,symbol ,initvalue ,docstring))
+
+(defmacro defparameter (symbol &optional initvalue docstring)
+  `(progn
+     (defvar ,symbol nil ,docstring)
+     (setq   ,symbol ,initvalue)))
+
+(defparameter th-frame-config-register ?°
+  "The register which is used for storing and restoring frame
+configurations by `th-save-frame-configuration' and
+`th-jump-to-register'.")
+
+(defun th-save-frame-configuration (arg)
+  "Stores the current frame configuration in register
+`th-frame-config-register'. If a prefix argument is given, you
+can choose which register to use."
+  (interactive "P")
+  (let ((register (if arg
+                      (read-char "Which register? ")
+                    th-frame-config-register)))
+    (frame-configuration-to-register register)
+    (message "Frame configuration saved in register '%c'."
+             register)))
+
+(defun th-jump-to-register (arg)
+  "Jumps to register `th-frame-config-register'. If a prefix
+argument is given, you can choose which register to jump to."
+  (interactive "P")
+  (let ((register (if arg
+                      (read-char "Which register? ")
+                    th-frame-config-register)))
+    (jump-to-register register)
+    (message "Jumped to register '%c'."
+             register)))
+
+(global-set-key (kbd "<f5>")
+                'th-save-frame-configuration)
+(global-set-key (kbd "<f6>")
+                'th-jump-to-register)
