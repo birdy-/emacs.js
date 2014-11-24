b91266af1157e738177e4986b8d711a7d6436082
Clément 'birdy' Danjou <clement.danjou@honeyvinegar.fr>
2014-11-24 12:09:03 +0100

    Update jshing for flymake dir

--- config/jshint.el
+++ config/jshint.el
@@ -3,4 +3,9 @@
 (require 'flymake-jshint)
 
 (add-hook 'js2-mode-hook
-     (lambda () (flymake-mode t)))
+          (lambda () (flymake-mode t)))
+
+; Nope, I want my copies in the system temp dir.
+(setq flymake-run-in-place nil)
+;; This lets me say where my temp dir is.
+(setq temporary-file-directory "~/.emacs.d/tmp/")
